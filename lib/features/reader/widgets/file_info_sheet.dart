b5a7e3f34f1be6c0202efea93d35c23a61f686be
Hiw4i <slava.vaneev8@gmail.com>
2026-09-19 16:46:57 +0300

    minor fix

--- lib/features/reader/widgets/file_info_sheet.dart
+++ lib/features/reader/widgets/file_info_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lucide_flutter/lucide_flutter.dart';
 
 import '../../../shared/theme/folio_theme.dart';
